a0db7db50b187b3f412a576c71a4acbc18c41e0c
NikolaiVerbitskii <verbitskiinikolai@gmail.com>
2017-02-02 18:02:34 +0300

    start task

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,70 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+      this.length = 0;
+      this._head = null;
+      this._tail = null;
+      console.info('constructor', this);
+    }
 
-    append(data) {}
+    append(data) {
+      console.info('append', 'this.length : ' + this.length, 'data : ' + data);
 
-    head() {}
+      let node,
+          currentIndex = this.length,
+          newIndex = this.length++;
 
-    tail() {}
+      if (currentIndex == 0) {
+        node = new Node();
+        this._head = node;
+        this._tail = node;
+      } else {
+        node = new Node(data, currentIndex, newIndex);
+        this._head = node;
+        this._tail = node;
+      }
 
-    at(index) {}
+      console.info('append end', this);
+    }
 
-    insertAt(index, data) {}
+    head() {
+      console.info('head', this);
+      return this._head;
+    }
 
-    isEmpty() {}
+    tail() {
+      console.info('tail', this);
+      return this._tail;
+    }
 
-    clear() {}
+    at(index) {
+      console.info('at', this);
+    }
 
-    deleteAt(index) {}
+    insertAt(index, data) {
+      console.info('insertAt', this);
+    }
 
-    reverse() {}
+    isEmpty() {
+      console.info('isEmpty', this);
+    }
 
-    indexOf(data) {}
+    clear() {
+      console.info('clear', this);
+    }
+
+    deleteAt(index) {
+      console.info('deleteAt', this);
+    }
+
+    reverse() {
+      console.info('reverse', this);
+    }
+
+    indexOf(data) {
+      console.info('indexOf', this);
+    }
 }
 
 module.exports = LinkedList;
